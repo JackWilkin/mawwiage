1cbaa8f1fb8bdb77cd35aeecd505e4f3a1169efa
Jack Wilkin <jackwilkin@jacks-mbp-2.mynetworksettings.com>
2025-02-04 01:30:05 -0500

    finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router";
 import styled from 'styled-components';
-import './styles/index.css';
 import { lavendar, mint } from './styles/colors';
 import HomePage from './pages/HomePage';
 import SaveTheDate from './pages/SaveTheDate';
