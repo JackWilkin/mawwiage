1a7b136ec166e1996da713e441e8abf7446c3954
Jack Wilkin <jackwilkin@jacks-mbp-2.mynetworksettings.com>
2025-02-03 23:39:23 -0500

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
+// import arch from './rose_may_flower_arch.jpg';
+// <img src={arch} alt="logo" />
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Homepage for Rose and May's wedding.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Mawwaige is rose + may
+        </a> */}
       </header>
     </div>
   );
